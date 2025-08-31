cde99c043c4bbfcc86f829ab356ba4176b8186a9
Felix Cruz <fcruzm95@gmail.com>
2025-08-31 22:05:46 +0100

    Formatting Output with a Pipe

--- src/app/investments-results/investments-results.component.ts
+++ src/app/investments-results/investments-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { InvestmentResults } from '../investment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investments-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investments-results.component.html',
   styleUrl: './investments-results.component.css'
 })
